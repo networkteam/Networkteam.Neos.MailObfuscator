9a998f511e3df716092d5aeca98b415745d3e283
Kai Möller <kai.moeller@networkteam.com>
2021-09-02 15:56:47 +0200

    remove return type declaration for compatibility reasons

--- Tests/Unit/Fusion/ConvertEmailLinksImplementationTest.php
+++ Tests/Unit/Fusion/ConvertEmailLinksImplementationTest.php
@@ -84,7 +84,7 @@ class ConvertEmailLinksImplementationTest extends UnitTestCase
         $this->assertSame($expectedText, $actualResult);
     }
 
-    public function emailTexts(): array
+    public function emailTexts()
     {
         return [
             'just some text not to touch' => [
